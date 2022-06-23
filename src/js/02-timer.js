9a1943d5e5e27a07fc000af677509e11e00b15f6
Rostyslav Marchenko <marchenko.r.7@gmail.com>
2022-06-23 15:55:20 +0300

    task 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,13 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    timer: document.querySelector(".timer"),
+    startBtn: document.querySelector("[data-start]"),
+    timePicker: document.querySelector("#datetime-picker"),
+    days: document.querySelector("[data-days]"),
+    hours: document.querySelector("[data-hours]"),
+    minutes: document.querySelector("[data-minutes]"),
+    seconds: document.querySelector("[data-seconds]"),
+}
+
